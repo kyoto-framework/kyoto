e9d12c22b32047be4a2d1fc0af787ec87e6a15c8
Yurii Zinets <yurii.zinets@icloud.com>
2021-09-17 11:42:16 +0200

    additional log

--- web/component.block.statistics.go
+++ web/component.block.statistics.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
 	ssc "github.com/yuriizinets/ssceng"
@@ -29,6 +30,9 @@ func (c *ComponentBlockStatistics) Async() error {
 	resp, err := http.Get("https://api.github.com/repos/" + c.Repo)
 	if err != nil {
 		return err
+	}
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("%v", resp.StatusCode)
 	}
 	repo := map[string]interface{}{}
 	err = json.NewDecoder(resp.Body).Decode(&repo)
