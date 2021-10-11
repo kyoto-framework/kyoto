debbbd7ed6f4bad1463d2333034a41d602b5b9c5
Yurii Zinets <yurii.zinets@icloud.com>
2021-10-11 14:01:48 +0200

    change tailwind conf

--- web/static/tailwind.config.js
+++ web/static/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: [
         "../*.go",
@@ -8,17 +10,7 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                gray: {
-                    '100': '#f5f5f5',
-                    '200': '#eeeeee',
-                    '300': '#e0e0e0',
-                    '400': '#bdbdbd',
-                    '500': '#9e9e9e',
-                    '600': '#757575',
-                    '700': '#616161',
-                    '800': '#424242',
-                    '900': '#212121',
-                }
+                gray: colors.trueGray
             }
         },
     },
