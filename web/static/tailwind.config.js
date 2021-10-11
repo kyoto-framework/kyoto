3ffdad3d2a7c411e208726028bc277b5084e60e6
Yurii Zinets <yurii.zinets@icloud.com>
2021-10-11 10:11:55 +0200

    change colors

--- web/static/tailwind.config.js
+++ web/static/tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     mode: 'jit',
     darkMode: 'media', // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                gray: {
+                    '100': '#f5f5f5',
+                    '200': '#eeeeee',
+                    '300': '#e0e0e0',
+                    '400': '#bdbdbd',
+                    '500': '#9e9e9e',
+                    '600': '#757575',
+                    '700': '#616161',
+                    '800': '#424242',
+                    '900': '#212121',
+                }
+            }
+        },
     },
     variants: {},
     plugins: [],
