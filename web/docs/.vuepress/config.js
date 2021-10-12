b0d6de3c773a75ba4a63fc9974b68cfd3af95632
Yurii Zinets <yurii.zinets@icloud.com>
2021-10-12 14:56:24 +0200

    fix link

--- web/docs/.vuepress/config.js
+++ web/docs/.vuepress/config.js
@@ -49,7 +49,7 @@ module.exports = {
                     },
                     {
                         text: 'Components',
-                        link: '/core/#components'
+                        link: '/core/#component'
                     },
                     {
                         text: 'Rendering',
