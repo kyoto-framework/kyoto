b086dcee8400544e3d3c697f0d461d0186e2028c
Yurii Zinets <yurii.zinets@icloud.com>
2021-10-15 12:17:00 +0200

    cached statistics

--- .web/component.block.statistics.go
+++ .web/component.block.statistics.go
@@ -4,9 +4,14 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/yuriizinets/kyoto"
 )
+
+// Statistics component cache
+var statisticsCache = ComponentBlockStatistics{}
+var statisticsTimestamp = time.Time{}
 
 // Repository statistics
 type ComponentBlockStatistics struct {
@@ -27,21 +32,31 @@ func (c *ComponentBlockStatistics) Init(p kyoto.Page) {
 }
 
 func (c *ComponentBlockStatistics) Async() error {
-	resp, err := http.Get("https://api.github.com/repos/" + c.Repo)
-	if err != nil {
-		return err
+	// Update cache (once in 3 hours)
+	if time.Since(statisticsTimestamp) > (3 * time.Hour) {
+		resp, err := http.Get("https://api.github.com/repos/" + c.Repo)
+		if err != nil {
+			return err
+		}
+		if resp.StatusCode != 200 {
+			return fmt.Errorf("%v", resp.StatusCode)
+		}
+		data := map[string]interface{}{}
+		err = json.NewDecoder(resp.Body).Decode(&data)
+		if err != nil {
+			return err
+		}
+		statisticsCache.Stars = int(data["stargazers_count"].(float64))
+		statisticsCache.Forks = int(data["forks_count"].(float64))
+		statisticsCache.Contributors = 3
+		statisticsCache.Sponsors = 1
+		statisticsTimestamp = time.Now()
 	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("%v", resp.StatusCode)
-	}
-	repo := map[string]interface{}{}
-	err = json.NewDecoder(resp.Body).Decode(&repo)
-	if err != nil {
-		return err
-	}
-	c.Stars = int(repo["stargazers_count"].(float64))
-	c.Forks = int(repo["forks_count"].(float64))
-	c.Contributors = 3
-	c.Sponsors = 1
+	// Set fields from cache
+	c.Stars = statisticsCache.Stars
+	c.Forks = statisticsCache.Forks
+	c.Contributors = statisticsCache.Contributors
+	c.Sponsors = statisticsCache.Sponsors
+	// Return
 	return nil
 }
