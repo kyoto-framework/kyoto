9914bac8a175bb72d63abd6f5b68ca36536f6772
Yurii Zinets <yurii.zinets@icloud.com>
2021-10-25 19:57:21 +0200

    remove docs preffix from sidebar

--- .web/docs/.vuepress/config.js
+++ .web/docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'kyoto',
     description: 'Keep your Go stack united with Server Side Components',
 
-    base: '/docs/',
+    base: '/',
 
     themeConfig: {
         navbar: [
@@ -45,81 +45,81 @@ module.exports = {
                 children: [
                     {
                         text: 'Structures',
-                        link: '/docs/concepts/#structures'
+                        link: '/concepts/#structures'
                     },
                     {
                         text: 'Rendering lifecycle',
-                        link: '/docs/concepts/#rendering-lifecycle'
+                        link: '/concepts/#rendering-lifecycle'
                     },
                     {
                         text: 'Lifecycle integration',
-                        link: '/docs/concepts/#lifecycle-integration'
+                        link: '/concepts/#lifecycle-integration'
                     },
                     {
                         text: 'Methods overloading',
-                        link: '/docs/concepts/#methods-overloading'
+                        link: '/concepts/#methods-overloading'
                     },
                 ]
             },
             {
                 text: 'Core Features',
-                link: '/docs/core-features',
+                link: '/core-features',
                 children: [
                     {
                         text: 'Page rendering',
-                        link: '/docs/core-features/#page-rendering'
+                        link: '/core-features/#page-rendering'
                     },
                     {
                         text: 'Built-in handler',
-                        link: '/docs/core-features/#built-in-handler'
+                        link: '/core-features/#built-in-handler'
                     },
                     {
                         text: 'Context management',
-                        link: '/docs/core-features/#context-management'
+                        link: '/core-features/#context-management'
                     },
                     {
                         text: 'Component lifecycle',
-                        link: '/docs/core-features/#component-lifecycle'
+                        link: '/core-features/#component-lifecycle'
                     }
                 ]
             },
             {
                 text: 'Extended Features',
-                link: '/docs/extended-features',
+                link: '/extended-features',
                 children: [
                     {
                         text: 'Server Side Actions',
-                        link: '/docs/extended-features/#server-side-actions',
+                        link: '/extended-features/#server-side-actions',
                         children: [
                             {
                                 text: 'Installation',
-                                link: '/docs/extended-features/#ssa-installation'
+                                link: '/extended-features/#ssa-installation'
                             },
                             {
                                 text: 'Usage',
-                                link: '/docs/extended-features/#ssa-usage'
+                                link: '/extended-features/#ssa-usage'
                             },
                             {
                                 text: 'Lifecycle',
-                                link: '/docs/extended-features/#ssa-lifecycle'
+                                link: '/extended-features/#ssa-lifecycle'
                             },
                             {
                                 text: 'Notes',
-                                link: '/docs/extended-features/#ssa-notes'
+                                link: '/extended-features/#ssa-notes'
                             }
                         ]
                     },
                     {
                         text: 'Server Side State',
-                        link: '/docs/extended-features/#server-side-state'
+                        link: '/extended-features/#server-side-state'
                     },
                     {
                         text: 'Meta builder',
-                        link: '/docs/extended-features/#meta-builder'
+                        link: '/extended-features/#meta-builder'
                     },
                     {
                         text: 'Insights',
-                        link: '/docs/extended-features/#insights'
+                        link: '/extended-features/#insights'
                     }
                 ]
             },
@@ -135,7 +135,7 @@ module.exports = {
             },
             {
                 text: 'Example with guide',
-                link: '/docs/example-with-guide'
+                link: '/example-with-guide'
             }
         ]
     },
