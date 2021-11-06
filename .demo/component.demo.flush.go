c04412beb925ee3bc9be2c2f18d88ee1e6e60773
Yurii Zinets <yurii.zinets@icloud.com>
2021-11-06 14:32:36 +0100

    add large payload for flush demo

--- .demo/component.demo.flush.go
+++ .demo/component.demo.flush.go
@@ -7,7 +7,16 @@ import (
 )
 
 type ComponentDemoFlush struct {
-	Status string
+	Payload string
+	Status  string
+}
+
+func (c *ComponentDemoFlush) Init() {
+	if c.Payload == "" {
+		for i := 0; i < 1000; i++ {
+			c.Payload += "SomeLongState"
+		}
+	}
 }
 
 func (c *ComponentDemoFlush) Actions(p kyoto.Page) kyoto.ActionMap {
