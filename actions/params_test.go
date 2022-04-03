ef71909652c29cbe3ffb7f2e3153da2f1dac8fdd
Yurii Zinets <yurii.zinets@icloud.com>
2022-04-03 14:21:52 +0200

    adapt params test

--- actions/params_test.go
+++ actions/params_test.go
@@ -1,37 +1,61 @@
 package actions
 
-import "testing"
+import (
+	"bytes"
+	"mime/multipart"
+	"net/http"
+	"testing"
+)
 
 // TestParseParameters ensures that the ParseParameters function works as expected
 func TestParseParameters(t *testing.T) {
-	path1 := "/internal/actions/ComponentFoo/eyJGb28iOiJCYXIifQ==/ActionName/W10="
-	path2 := "/custom-route/ComponentFoo/eyJGb28iOiJCYXIifQ==/ActionName/W10="
-
-	params1, err := ParseParameters(path1)
+	mpb1 := bytes.Buffer{}
+	mpw1 := multipart.NewWriter(&mpb1)
+	mpw1.WriteField("State", `{"Foo":"Bar"}`)
+	mpw1.WriteField("Args", "[]")
+	mpw1.Close()
+	req1, err := http.NewRequest("POST", "http://localhost:8080/internal/actions/ComponentFoo/ActionName", &mpb1)
+	req1.Header.Set("Content-Type", mpw1.FormDataContentType())
 	if err != nil {
-		t.Errorf("ParseParameters(%s) returned error: %v", path1, err)
+		panic(err)
+	}
+
+	mpb2 := bytes.Buffer{}
+	mpw2 := multipart.NewWriter(&mpb2)
+	mpw2.WriteField("State", `{"Foo":"Bar"}`)
+	mpw2.WriteField("Args", "[]")
+	mpw2.Close()
+	req2, err := http.NewRequest("POST", "http://localhost:8080/custom-route/ComponentFoo/ActionName", &mpb2)
+	req2.Header.Set("Content-Type", mpw2.FormDataContentType())
+	if err != nil {
+		panic(err)
+	}
+
+	params1, err := ParseParameters(req1)
+	if err != nil {
+		t.Errorf("ParseParameters(%s) returned error: %v", "req1", err)
 	}
 	if params1.Component != "ComponentFoo" {
-		t.Errorf("ParseParameters(%s) returned wrong component: %s", path1, params1.Component)
+		t.Errorf("ParseParameters(%s) returned wrong component: %s", "req1", params1.Component)
 	}
 	if params1.Action != "ActionName" {
-		t.Errorf("ParseParameters(%s) returned wrong action: %s", path1, params1.Action)
+		t.Errorf("ParseParameters(%s) returned wrong action: %s", "req1", params1.Action)
 	}
 	if params1.State["Foo"] != "Bar" {
-		t.Errorf("ParseParameters(%s) returned wrong state: %v", path1, params1.State)
+		t.Errorf("ParseParameters(%s) returned wrong state: %v", "req1", params1.State)
 	}
 
-	params2, err := ParseParameters(path2)
+	params2, err := ParseParameters(req2)
 	if err != nil {
-		t.Errorf("ParseParameters(%s) returned error: %v", path2, err)
+		t.Errorf("ParseParameters(%s) returned error: %v", "req2", err)
 	}
 	if params2.Component != "ComponentFoo" {
-		t.Errorf("ParseParameters(%s) returned wrong component: %s", path2, params2.Component)
+		t.Errorf("ParseParameters(%s) returned wrong component: %s", "req2", params2.Component)
 	}
 	if params2.Action != "ActionName" {
-		t.Errorf("ParseParameters(%s) returned wrong action: %s", path2, params2.Action)
+		t.Errorf("ParseParameters(%s) returned wrong action: %s", "req2", params2.Action)
 	}
 	if params2.State["Foo"] != "Bar" {
-		t.Errorf("ParseParameters(%s) returned wrong state: %v", path2, params2.State)
+		t.Errorf("ParseParameters(%s) returned wrong state: %v", "req2", params2.State)
 	}
 }
