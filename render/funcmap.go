31405857ee63578c9f57b576fb66d3919a5ecbaf
Yurii Zinets <yurii.zinets@icloud.com>
2022-04-08 11:30:57 +0200

    funcmap composition function

--- render/funcmap.go
+++ render/funcmap.go
@@ -2,6 +2,17 @@ package render
 
 import "html/template"
 
+// ComposeFuncMap is a function for composing multiple FuncMap instances into one
+func ComposeFuncMap(fmaps ...template.FuncMap) template.FuncMap {
+	var result = template.FuncMap{}
+	for _, fmap := range fmaps {
+		for k, v := range fmap {
+			result[k] = v
+		}
+	}
+	return result
+}
+
 // FuncMap is responsible for integration of library functionality into template rendering
 // You need to use this function while template building (or mix with your own)
 func FuncMap() template.FuncMap {
