99ea6b55f0e8e798e8b7dec4e42d3fc437543db5
Yurii Zinets <yurii.zinets@icloud.com>
2024-01-04 00:10:35 +0100

    more documentation

--- component/state.disposable.go
+++ component/state.disposable.go
@@ -1,13 +1,19 @@
 package component
 
+// Disposable component means that
+// we don't want to store state or use dynamic actions.
+// On action call you'll get explicit error about wrong usage.
 type Disposable struct {
 	Name
 }
 
+// Marshal for disposable returns "disposable" string.
+// This flag will help to detect wrong action usage.
 func (*Disposable) Marshal() string {
 	return "disposable"
 }
 
+// Unmarshal for disposable returns nothing.
 func (*Disposable) Unmarshal(str string) {
 	return
 }
--- doc.go
+++ doc.go
@@ -59,13 +59,109 @@ and https://pkg.go.dev for Go'ish documentation style.
 We don't want you to deal with boilerplate code on your own,
 so you can proceed with our simple starter project.
 
-	git clone https://kyoto.codes/new <your-new-project> && rm -r <your-new-project>/.git
+	$ git clone https://kyoto.codes/new <your-new-project>
+	$ rm -r <your-new-project>/.git
 
-Anyway, if you want a bare-minimum,
-single file example right now,
-we're also providing a code snippet below.
-It includes a page with multiple expensive to load components with dynamic behavior (actions).
-It's documented in detail,
-but not includes all the existing features of library.
+Feel free to use it as an example for your own setup.
+
+# Components
+
+Components is a common approach for modern libraries to manage frontend parts.
+Kyoto components are trying to be mostly independent (but configurable) part of the project.
+
+To create component, it would be enough to implement component.Component.
+It's a function, a context receiver which returns a component state.
+State is an implementation of component.State,
+which is easy to implement with nesting one of the state implementations (options will be described later).
+
+	package main
+
+	type ComponentState struct {
+		component.Disposable // You're providing component with some abilities here
+	}
+
+	func Component(ctx *component.Context) component.State {
+		state := &ComponentState{}
+		return state
+	}
+
+	...
+
+Each component becomes a part of the page or top-level component,
+which executes component function asynchronously and gets a state future object.
+In that way your components are executing in a non-blocking way.
+
+Pages are just top-level components, where you can configure rendering and page related stuff.
+
+# Components with state
+
+Stateful components are pretty similar to stateless ones,
+but they are actualy implementing marshal/unmarshal interface instead of mocking it.
+
+You have multiple state options to choose from: universal or server.
+
+Universal state is a state, that can be marshalled and unmarshalled both on server and client.
+It's a common state option without functionality limitations.
+On the other hand, the whole state must to be sent and received,
+which applies some limitations on the state size.
+
+	package main
+
+	type ComponentState struct {
+		component.Universal // This state allows you to operate with data on both server and client
+	}
+
+	func Component(ctx *component.Context) component.State {
+		state := &ComponentState{}
+		return state
+	}
+
+Server state can be marshalled and unmarshalled only on server.
+It's a good option for components, that are not supposed to be updated on client side (f.e. no inputs).
+Also, it's a good option for components with lots of state data.
+
+	package main
+
+	type ComponentState struct {
+		component.Server // This state allows you to operate with data on server only
+	}
+
+	func Component(ctx *component.Context) component.State {
+		state := &ComponentState{}
+		return state
+	}
+
+# Components with arguments
+
+Sometimes you may want to pass some arguments to the component.
+It's easy to do with wrapping component with additional function.
+
+	package main
+
+	type ComponentState struct {
+		component.Universal
+
+		Data string
+	}
+
+	func Component(data string) component.Component {
+		return func(ctx *component.Context) component.State {
+			state := &ComponentState{}
+			state.Data = data // We are passing arg to the component state, but it's not a requirement.
+			return state
+		}
+	}
+
+# Rendering
+
+...
+
+# Routing
+
+...
+
+# Actions
+
+...
 */
 package kyoto
